ca9568e0cce5503a0c7d55b6cbfacf41a621ea48
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2024-02-22 17:38:50 +0100

    feat(roles): add helpers for adding/removing roles

---
+++ src/lib/roles.ts
@@ -0,0 +1,74 @@
+import { Guild, User } from "discord.js";
+import { DoraException } from "./exceptions/DoraException";
+
+const getRole = async ({ guild, roleId }: { guild: Guild; roleId: string }) => {
+  try {
+    return await guild?.roles.fetch(roleId);
+  } catch (err) {
+    throw new DoraException(
+      `Failed to fetch role '${roleId}' in guild '${guild.name}'`,
+      DoraException.Type.Unknown,
+      { cause: err },
+    );
+  }
+};
+
+export const addRole = async ({
+  roleId,
+  guild,
+  user,
+}: {
+  roleId: string;
+  guild: Guild;
+  user: User;
+}) => {
+  const role = await getRole({ guild, roleId });
+  if (!role) {
+    throw new DoraException(
+      `Unable to find role '${roleId}' in guild '${guild.name}' to add to user '${user.displayName}'`,
+      DoraException.Type.NotFound,
+    );
+  }
+
+  try {
+    await guild?.members.addRole({ role, user });
+  } catch (err) {
+    throw new DoraException(
+      `Failed to add role '${role.name}' in guild '${guild.name}' to user '${user.displayName}'`,
+      DoraException.Type.Unknown,
+      { cause: err },
+    );
+  }
+
+  return role;
+};
+
+export const removeRole = async ({
+  roleId,
+  guild,
+  user,
+}: {
+  roleId: string;
+  guild: Guild;
+  user: User;
+}) => {
+  const role = await getRole({ guild, roleId });
+  if (!role) {
+    throw new DoraException(
+      `Unable to find role '${roleId}' in guild '${guild.name}' to remove from user '${user.displayName}'`,
+      DoraException.Type.NotFound,
+    );
+  }
+
+  try {
+    await guild?.members.removeRole({ role, user });
+  } catch (err) {
+    throw new DoraException(
+      `Failed to remove role '${role.name}' in guild '${guild.name}' from user '${user.displayName}'`,
+      DoraException.Type.Unknown,
+      { cause: err },
+    );
+  }
+
+  return role;
+};
