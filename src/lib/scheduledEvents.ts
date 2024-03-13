62c53b03876214f44f5e2148bc787707b88d52d4
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2024-03-13 21:02:02 +0100

    fix(reminders): fix markdown links in reminders with emojis

--- src/lib/scheduledEvents.ts
+++ src/lib/scheduledEvents.ts
@@ -131,7 +131,17 @@ const announceScheduledEventIfRelevant = async ({
   );
   const subscribers = await scheduledEvent.fetchSubscribers();
   const interestedUsers = subscribers.map((subscriber) => subscriber.user);
+  const nameWithoutEmojis = removeEmojis(scheduledEvent.name); // Emojis break the markdown link
   await channel.send(
-    `[${scheduledEvent.name}](${scheduledEvent.toString()}) will take place in ${hoursAway} hours. Currently set as interested: ${interestedUsers.join(" ")}`,
+    `[${nameWithoutEmojis}](${scheduledEvent.url}) will take place in ${hoursAway} hours. Currently set as interested: ${interestedUsers.join(" ")}`,
   );
+};
+
+const removeEmojis = (input: string) => {
+  // Stolen from here: https://stackoverflow.com/a/40763403
+  const regex =
+    /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*/g;
+  const withoutEmojis = input.replace(regex, "");
+  const withoutDoubleSpaces = withoutEmojis.replace("  ", " "); // <text> <emoji> <text> will result in double spaces
+  return withoutDoubleSpaces.trim();
 };
