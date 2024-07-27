9010625868a4ee832dbf146e40a1f6416a886987
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2024-07-27 23:26:04 +0200

    chore(cron): make it easy to switch between cron intervals

--- src/cronJobs.ts
+++ src/cronJobs.ts
@@ -2,9 +2,17 @@ import schedule from "node-schedule";
 import { logger } from "./lib/logger";
 import { announceRelevantScheduledEventsForAllGuilds } from "./lib/discord/scheduledEvents";
 
+/**
+ * Interval options for cron job. A scheduled job with the minute format
+ * will run every minute, and the hour format will run every hour etc
+ */
+const CRON_INTERVAL = {
+  MINUTE: "* * * * *", // Helpful when running locally to test an action
+  HOUR: "0 * * * *",
+};
+
 export const registerCronJobs = () => {
-  // Run every hour
-  schedule.scheduleJob("0 * * * *", async () => {
+  schedule.scheduleJob(CRON_INTERVAL.HOUR, async () => {
     logger.info("Running hourly cron job");
     await announceRelevantScheduledEventsForAllGuilds();
   });
