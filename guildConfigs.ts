8cee3f7219f01016e2d553b4a9d2707c027089e8
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2024-08-18 15:45:37 +0200

    chore: guild config improvements

--- guildConfigs.ts
+++ guildConfigs.ts
@@ -3,18 +3,34 @@ import type { PiiFieldName } from "~/modals/piiModal";
 type GuildConfig = {
   guildId: string;
   piiFields: PiiFieldName[] | "all";
+  channelIds: {
+    birthdayWishes: string;
+  };
 };
-export const guildConfigs = {
-  localBotTesting: {
+export const guildConfigs: { [guildId: string]: GuildConfig } = {
+  // Local bot testing
+  "1211309484811485264": {
     guildId: "1211309484811485264",
     piiFields: "all",
+    channelIds: {
+      /** Where to wish users happy birthday */
+      birthdayWishes: "1216485497501908992", // #dora-test
+    },
   },
-  climbing: {
+  // Climbing (Dora the Explorer)
+  "1193809867232772126": {
     guildId: "1193809867232772126",
     piiFields: ["birthdayInput"],
+    channelIds: {
+      birthdayWishes: "1193989101599326259", // #all-chat
+    },
   },
-  eithon: {
+  // Eithon
+  "106099890320330752": {
     guildId: "106099890320330752",
     piiFields: ["birthdayInput", "firstNameInput", "heightInput"],
+    channelIds: {
+      birthdayWishes: "106099890320330752", // #general
+    },
   },
-} satisfies Record<string, GuildConfig>;
+};
