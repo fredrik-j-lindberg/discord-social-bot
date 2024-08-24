a3335bf313df6c18b4807d69766eb3441a4e0961
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2024-08-24 08:55:20 +0200

    chore(birthdays): add debug command

---
+++ src/router/commands/debugBirthday.ts
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder } from "discord.js";
+import type { Command } from "../commandRouter";
+import { happyBirthday } from "~/cron/happyBirthday";
+
+export default {
+  deferReply: false,
+  data: new SlashCommandBuilder()
+    .setName("debugbirthday")
+    .setDescription(
+      "Should remove role from existing members, and re-add it to anyone whose birthday is today",
+    ),
+  execute: async (interaction) => {
+    await happyBirthday();
+    await interaction.reply("Ran happy birthday cron manually");
+  },
+} satisfies Command;
