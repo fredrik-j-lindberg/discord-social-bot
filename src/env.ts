628c064ede4c1c547538d585e3cc4d2e9f23a33a
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2024-11-29 22:45:06 +0100

    env: make it easy to re-use boolean schema

--- src/env.ts
+++ src/env.ts
@@ -1,18 +1,19 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
+const booleanSchema = z
+  .string()
+  // only allow "true" or "false"
+  .refine((s) => s === "true" || s === "false")
+  // transform to boolean
+  .transform((s) => s === "true");
+
 export const env = createEnv({
   server: {
     DISCORD_BOT_TOKEN: z.string(),
     DISCORD_BOT_CLIENT_ID: z.string(),
     AIRTABLE_API_KEY: z.string(),
-    USE_DEV_GUILD_CONFIGS: z
-      .string()
-      // only allow "true" or "false"
-      .refine((s) => s === "true" || s === "false")
-      // transform to boolean
-      .transform((s) => s === "true")
-      .optional(),
+    USE_DEV_GUILD_CONFIGS: booleanSchema.optional(),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
